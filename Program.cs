3e6784d297f2146fedbfedeb9534c7f0da78de7a
MinhNhut <lmnhut0211@gmai.com>
2023-12-26 14:24:31 +0700

    final

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     string greeting;
+        Console.WriteLine("Nhap HelloWorld");
+        greeting = Console.ReadLine();
+        Console.WriteLine(greeting);
+    }
+  }
+}
