9623dc6a1a9c4a133523f1c3c445899918db6576
MinhNhut <lmnhut0211@gmai.com>
2023-12-26 14:33:42 +0700

    final4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿//Bai 1 Input
+  string greeting;
+  Console.WriteLine("Nhap HelloWorld");
+  greeting = Console.ReadLine();
+  Console.WriteLine(greeting);  
 
-namespace MyApplication
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-     string greeting;
-        Console.WriteLine("Nhap HelloWorld");
-        greeting = Console.ReadLine();
-        Console.WriteLine(greeting);
-    }
-  }
-}
+//Bai 1-1 Output
+ Console.WriteLine("Hello World");  
+
+ //Bai 2
+
+ string a = "HelloWorld";
+ Console.WriteLine(a);
+
+ //Bai 3
+
+ string firstLetter = "Hello";
+ string c = " ";
+ string lastLetter = "World";
+ Console.WriteLine(firstLetter + c + lastLetter);
+
+
+ //Bai 4
+
+   string word = string.Concat(firstLetter,c , lastLetter);
+   Console.WriteLine(word);
